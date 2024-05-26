dd851d53c31db58f9850441549078446cf8813ee
Huy010302 <111289985+Huy010302@users.noreply.github.com>
2024-05-26 22:26:50 +0700

    fix level 3, fix display of enemy

--- src/uet/oop/bomberman/BombermanGame.java
+++ src/uet/oop/bomberman/BombermanGame.java
@@ -4,7 +4,7 @@ import uet.oop.bomberman.gui.Frame;
 import uet.oop.bomberman.sound.Sound;
 
 public class BombermanGame {
-	
+
 	public static void main(String[] args) {
 		Sound.play("soundtrack");
 		new Frame();
