5cd832d40c57c47b38346547c16532de67cdda64
Huy010302 <111289985+Huy010302@users.noreply.github.com>
2024-05-31 00:51:45 +0700

    fix collide for player 1

--- src/uet/oop/bomberman/entities/character/Bomber2.java
+++ src/uet/oop/bomberman/entities/character/Bomber2.java
@@ -11,13 +11,6 @@ public class Bomber2 extends Bomber {
         super(x, y, baseSpeed, baseBombLimit, baseBombRadius, entityManager);
     }
 
-    // @Override
-    // public boolean collide(Entity e) {
-    // if (!super.collide(e))
-    // return false;
-    // return true;
-    // }
-
     @Override
     protected void chooseSprite() {
         switch (getDirection()) {
